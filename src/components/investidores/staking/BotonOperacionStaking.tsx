23bf9028ed0332e85b2e798c0b683a2e8a8a271f
Rodrigo <rodrigocarrion93@gmail.com>
2023-04-22 20:56:14 -0300

    Correcion del vaultBtcb

--- src/components/investidores/staking/BotonOperacionStaking.tsx
+++ src/components/investidores/staking/BotonOperacionStaking.tsx
@@ -3,10 +3,10 @@ import { AppDispatch } from "../../../store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { ethers } from "ethers";
 interface BotonOperacionProps {
-  balanceUserAmt: string | null;
-  stackedByUser: string | null;
-  btcACobrar: string | null;
-  allowance: string | null;
+  balanceUserAmt: number | undefined;
+  stackedByUser: number | undefined;
+  btcACobrar: number | undefined;
+  allowance: number | undefined;
   operacionAprobar: Function;
   operacionStake: Function;
   input: string;
@@ -21,13 +21,17 @@ const BotonOperacion: React.FC<BotonOperacionProps> = ({
   operacionStake,
 }) => {
   const mesajeBoton = () => {
-    if (parseInt(allowance) < parseInt(input)) {
-      return "Aprobar";
-    }
-    if (parseInt(ethers.utils.formatEther(balanceUserAmt)) < parseInt(input)) {
-      return "Balance insuf.";
-    } else {
-      return "Stake";
+    console.log();
+
+    if (allowance && balanceUserAmt) {
+      if (allowance < parseFloat(input)) {
+        return "Aprobar";
+      }
+      if (balanceUserAmt < parseFloat(input)) {
+        return "Balance insuf.";
+      } else {
+        return "Stake";
+      }
     }
   };
 
@@ -36,7 +40,7 @@ const BotonOperacion: React.FC<BotonOperacionProps> = ({
     <button
       onClick={() => {
         if (allowance) {
-          parseInt(allowance) > parseInt(input)
+          allowance > parseFloat(input)
             ? operacionStake(dispatch, input)
             : operacionAprobar(dispatch);
         }
