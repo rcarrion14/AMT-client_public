f005781b5ef2ec237330ad42bb9b40566ed8e033
Kegnell <diego.pereira.pck@gmail.com>
2023-05-13 19:57:43 -0300

    fixing ts checks

--- src/components/Investimentos/liquidez/AlertaStakingLiquidez.tsx
+++ src/components/Investimentos/liquidez/AlertaStakingLiquidez.tsx
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
--- src/components/Investimentos/liquidez/Liquidez.tsx
+++ src/components/Investimentos/liquidez/Liquidez.tsx
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import React, { useState } from "react";
 import { textoLiquidez, textosExtra } from "../../../Utils/textos";
 import CuadroProveerLiquidez from "./CuadroProveerLiquidez";
