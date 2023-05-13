76f9fcab9e79574898b42c6e599fcc1b9fcf1e64
Kegnell <diego.pereira.pck@gmail.com>
2023-05-13 13:35:27 -0300

    navBar remove ts nocheck

--- src/components/Generales/NavBar.tsx
+++ src/components/Generales/NavBar.tsx
@@ -1,36 +1,37 @@
-// @ts-nocheck
+interface NavBarProps {
+  setActivePage: (page: string) => void;
+  activePage: string;
+}
 
-import React, { useState } from "react";
-
-const NavBar = ({ setActivePage, activePage }) => {
+const NavBar: React.FC<NavBarProps> = ({ setActivePage, activePage }) => {
   return (
     <div className="navBar">
       <img
         onClick={() => {
           setActivePage("home");
         }}
-        className={activePage == "home" ? "activeIcon" : null}
+        className={activePage == "home" ? "activeIcon" : undefined}
         src="icon_home.png"
       />
       <img
         onClick={() => {
           setActivePage("marketplace");
         }}
-        className={activePage == "marketplace" ? "activeIcon" : null}
+        className={activePage == "marketplace" ? "activeIcon" : undefined}
         src="icon_marketplace.png"
       />
       <img
         onClick={() => {
           setActivePage("investidores");
         }}
-        className={activePage == "investidores" ? "activeIcon" : null}
+        className={activePage == "investidores" ? "activeIcon" : undefined}
         src="icon_invest.png"
       />
       <img
         onClick={() => {
           setActivePage("gInvestidores");
         }}
-        className={activePage == "gInvestidores" ? "activeIcon" : null}
+        className={activePage == "gInvestidores" ? "activeIcon" : undefined}
         src="icon_grandesInvest.png"
       />
     </div>
