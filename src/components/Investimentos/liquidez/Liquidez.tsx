02ce0b4cb0812b0bf9cc99bacfc8f9a1326582a9
Kegnell <diego.pereira.pck@gmail.com>
2023-05-15 15:49:11 -0300

    add ts-nocheck to check run build of others components

--- src/components/Investimentos/liquidez/Liquidez.tsx
+++ src/components/Investimentos/liquidez/Liquidez.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useState } from "react";
 import { textoLiquidez, textosExtra } from "../../../Utils/textos";
 import CuadroProveerLiquidez from "./CuadroProveerLiquidez";
