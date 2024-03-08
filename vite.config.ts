5ac7875253ac97d223766ce0dfce4e3844cdd00d
Kegnell <diego.pereira.pck@gmail.com>
2024-03-08 12:07:09 -0300

    debug with sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "amt-dk",
+    project: "node"
+  })],
+
   server: {
     host: true,
   },
+
+  build: {
+    sourcemap: true
+  }
 });
